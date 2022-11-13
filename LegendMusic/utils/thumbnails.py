c0883062c339b4be1536e2060e9de76d28878095
TEAMSTD09 <110489709+TEAMSTD09@users.noreply.github.com>
2022-11-13 21:14:07 +0530

    Update thumbnails.py

--- LegendMusic/utils/thumbnails.py
+++ LegendMusic/utils/thumbnails.py
@@ -122,7 +122,7 @@ async def gen_thumb(videoid):
             font4 = ImageFont.truetype('assets/font2.ttf', 35)
 
             image4 = ImageDraw.Draw(image2)
-            image4.text((10, 10), "LEGEND X MUSIC", fill="white", font = font1, align ="left") 
+            image4.text((10, 10), "GARMI MUSIC", fill="white", font = font1, align ="left") 
             image4.text((670, 150), "๛⚘𝐌𝐢𝐬𝐬᭄๛𝐃𝐨𝐥𝐥°°°༄ PLAYER", fill="white", font = font2, stroke_width=2, stroke_fill="white", align ="left") 
 
             # title
