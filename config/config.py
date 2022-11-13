c75aa79736381e0992a0a568178664bb8d65660f
TEAMSTD09 <110489709+TEAMSTD09@users.noreply.github.com>
2022-11-13 20:48:23 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -45,7 +45,7 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 SUPPORT_CHANNEL = getenv(
     "SUPPORT_CHANNEL", "https://t.me/yaarokiyaariHRANDA")
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "https://t.me/YaaroKiMehfil_YKM")
+    "SUPPORT_GROUP", "https://t.me/DXLUCKYGROUP")
 
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False")
 
